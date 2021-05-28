b28d8c9665c4cb9ad1b8e18e4beaf43314cd268d
BenjaminANgafua <benjaminarkutl2017@gmail.com>
2021-05-28 10:12:11 +0000

    Euclid's GCD Algorithm

---
+++ script.js
@@ -0,0 +1,20 @@
+function checkGcd(lengt, widt) {
+    lengt = Number(document.querySelector("#length").value)
+    widt = Number(document.querySelector("#width").value)
+
+    var answer = euclidGcdAlgorithm(lengt, widt);
+    console.log(answer)
+    document.querySelector("#gcd").value = answer
+}
+
+
+function euclidGcdAlgorithm(len, wid) {
+    if (wid === 0) {
+        return len;
+    } else {
+        var remainder = len % wid;
+
+        //The gcd
+        return euclidGcdAlgorithm(wid, remainder)
+    }
+}
