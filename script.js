22d98418500142c0c23729394e0df55042d56fb9
BenjaminANgafua <benjaminarkutl2017@gmail.com>
2021-05-29 19:11:20 +0000

    Euclid's GCD Algorithm recent update

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
- function check_gcd(lengt, widt) {
-     lengt = Number(document.querySelector("#length").value)
-     widt = Number(document.querySelector("#width").value)
+//Take two inputs values and return the gcd of the two
+document.querySelector("#findGcd").addEventListener("click", (lengt, widt) => {
+    // Input value
+    lengt = Number(document.querySelector("#length").value)
+    widt = Number(document.querySelector("#width").value)
 
-     var answer = euclidGcdAlgorithm(lengt, widt);
-     console.log(answer)
-     document.querySelector("#gcd").value = answer
- }
+    // the print of the gcd
+    var answer = findGcd(lengt, widt);
+    document.querySelector("#gcd").value = answer
+})
 
+function findGcd(len, wid) {
+    // the base case to quit the program as the remainder be 0
+    if (wid === 0) {
+        return len;
+    } else {
+        //  Get the the remainder of  length and width
+        var modulus = len % wid;
 
- function euclidGcdAlgorithm(len, wid) {
-     if (wid === 0) {
-         return len;
-     } else {
-         var remainder = len % wid;
-
-         //The gcd
-         return euclidGcdAlgorithm(wid, remainder)
-     }
- }
+        //Keep calling the length and width until the width remain 0
+        return findGcd(wid, modulus)
+    }
+}
